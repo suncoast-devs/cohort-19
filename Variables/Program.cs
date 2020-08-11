0fa17fcaa4393136f7490cb7750062f5cdfb93b4
Gavin Stark <gavin@gstark.com>
2020-08-11 11:39:39 -0400

    Varialbes Example

---
+++ Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my program");
+
+            Console.WriteLine("\n\n\n\n\n\n\n\n");
+
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"It is a pleasure to meet you, {name}");
+            Console.WriteLine($"Your name has {name.Length} characters in it!");
+
+            var score = 98;
+            Console.WriteLine($"Twice your score is {2 * score - 5}");
+        }
+    }
+}
