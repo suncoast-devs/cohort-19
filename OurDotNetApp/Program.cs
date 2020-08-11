decbe79493f98375cee3e12cb22ae802500c75d9
Gavin Stark <gavin@gstark.com>
2020-08-11 08:18:24 -0400

    Our first C# application

---
+++ OurDotNetApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OurDotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#. My name is Gavin. I am a student at SDG.");
+        }
+    }
+}
