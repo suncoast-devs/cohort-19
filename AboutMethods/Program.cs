729420059e109c0c894c8cb0d94c9a0d76a707de
Gavin Stark <gavin@gstark.com>
2020-08-13 11:15:32 -0400

    Basic code with one method called DisplayGreeting

---
+++ AboutMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AboutMethods
+{
+    class Program
+    {
+        static void DisplayGreeting()
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Welcome to Our Employee Database");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            DisplayGreeting();
+
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+
+            Console.Write("What is your department number? ");
+            // Read in a string from the user
+            var departmentString = Console.ReadLine();
+            // But store the department number as an "int"
+            var department = int.Parse(departmentString);
+
+            Console.Write("What is your yearly salary (in dollars)? ");
+            var salary = int.Parse(Console.ReadLine());
+
+            var salaryPerMonth = salary / 12;
+            Console.WriteLine($"Hello, {name} you make {salaryPerMonth} a month.");
+
+        }
+    }
+}
