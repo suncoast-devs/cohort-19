3a3b8bdc5b67804c1c0461f71a05aa66856f4903
Gavin Stark <gavin@gstark.com>
2020-08-19 10:28:30 -0400

    Implemented the welcome message

--- PetAdoption/Program.cs
+++ PetAdoption/Program.cs
@@ -2,11 +2,27 @@
 
 namespace PetAdoption
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            // Welcome the user to the application
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Welcome to our Pet Adoption Agency");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // While the user hasn't quit the application
+
+            // Show them a menu of options they can do
+
+            // - Add a new pet
+            // - See all the pets up for adoption
+            // - Input a name of a pet that has been adopted to remove them from the list
+            // - Input a name of a pet and update their size
+            // - Quit the application
+
+        }
     }
-  }
 }
