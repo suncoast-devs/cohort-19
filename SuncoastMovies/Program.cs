fe15bab0aa51002aed2ebbac5f33653571900955
Gavin Stark <gavin@gstark.com>
2020-08-27 10:23:25 -0400

    Adds a Movie class and a database context to acces it

--- SuncoastMovies/Program.cs
+++ SuncoastMovies/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace SuncoastMovies
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Movie
     {
-      Console.WriteLine("Welcome to C#");
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PrimaryDirector { get; set; }
+        public int YearReleased { get; set; }
+        public string Genre { get; set; }
     }
-  }
+
+    // Define a database context for our Suncoast Movies database.
+    // It derives from (has a parent of) DbContext so we get all the
+    // abilities of a database context from EF Core.
+    class SuncoastMoviesContext : DbContext
+    {
+        // Define a movies property that is a DbSet.
+        public DbSet<Movie> Movies { get; set; }
+
+        // Define a method required by EF that will configure our connection
+        // to the database.
+        //
+        // DbContextOptionsBuilder is provided to us. We then tell that object
+        // we want to connect to a postgres database named suncoast_movies on
+        // our local machine.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;database=SuncoastMovies");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
+    }
 }
