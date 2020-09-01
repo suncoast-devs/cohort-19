190ceeb98fae1ab5e72f705a442e99f31feab611
Gavin Stark <gavin@gstark.com>
2020-09-01 10:07:22 -0400

    Adds an Item class that will store our JSON objects

--- OneListClient/Program.cs
+++ OneListClient/Program.cs
@@ -3,6 +3,15 @@ using System.Net.Http;
 
 namespace OneListClient
 {
+    class Item
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public bool complete { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+    }
+
     class Program
     {
         static async System.Threading.Tasks.Task Main(string[] args)
