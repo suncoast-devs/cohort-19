09efc6f596a9f1a301501346fda82453a8fe1b78
Gavin Stark <gavin@gstark.com>
2020-09-01 10:13:39 -0400

    Deserialize the JSON we get back from the API and turn it into a List of Item objects.
    Then print them out

--- OneListClient/Program.cs
+++ OneListClient/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.Json;
 
 namespace OneListClient
 {
@@ -18,9 +20,19 @@ namespace OneListClient
         {
             var client = new HttpClient();
 
-            var responseAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=sdg-handbook");
+            // Getting the response of the API as a stream of data
+            var responseAsStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token=sdg-handbook");
 
-            Console.WriteLine(responseAsString);
+            // Supply that *stream of data* to a Deserialize that will interpret it as a List of Item objects.
+            List<Item> items = await JsonSerializer.DeserializeAsync<List<Item>>(responseAsStream);
+
+            // For each item in our deserialized List of Item
+            foreach (var item in items)
+            {
+                // Output some details on that item
+                Console.WriteLine($"The task {item.text} was created on {item.created_at} and has a completion of: {item.complete}");
+            }
         }
     }
 }
+
