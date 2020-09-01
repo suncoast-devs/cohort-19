b7590764206406d2f0977766d2d6ecfd3f8fda81
Gavin Stark <gavin@gstark.com>
2020-09-01 10:18:18 -0400

    Change the updated and created at fields to DateTime so they PARSE nicely

--- OneListClient/Program.cs
+++ OneListClient/Program.cs
@@ -10,8 +10,8 @@ namespace OneListClient
         public int id { get; set; }
         public string text { get; set; }
         public bool complete { get; set; }
-        public string created_at { get; set; }
-        public string updated_at { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
     }
 
     class Program
