e778d1dc5ab7b6d8f398e84830b2fb73a4a4b6c2
Gavin Stark <gavin@gstark.com>
2020-09-03 09:54:56 -0400

    Define the model FIRST so we can use MIGRATIONS

---
+++ GameDatabaseAPI/Models/Game.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace GameDatabaseAPI.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Host { get; set; }
+        public string Address { get; set; }
+        public DateTime When { get; set; }
+        public int MinimumPlayers { get; set; }
+        public int MaximumPlayers { get; set; }
+
+    }
+}
