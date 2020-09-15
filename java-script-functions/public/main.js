e954b79c0503647d8b491e09811782b392bf1619
Gavin Stark <gavin@gstark.com>
2020-09-15 10:46:52 -0400

    Using methods as arguments.
    This makes printIt a flexible function that can do different kinds of work

--- java-script-functions/public/main.js
+++ java-script-functions/public/main.js
@@ -18,3 +18,29 @@ console.log(squaredFavorite)
 
 const anotherSquare = squaredAgain(100)
 console.log(anotherSquare)
+
+function printIt(array, func) {
+  // Go through the array one index at a time
+  for (let index = 0; index < array.length; index++) {
+    // Get the value at that index from the array
+    const value = array[index]
+
+    // call the function we were given in the
+    // argument named "func" and put the output
+    // of that in a variable called result
+    const result = func(value)
+
+    console.log(`Function turned ${value} into ${result}`)
+  }
+}
+
+const double = function (number) {
+  return number * 2
+}
+
+const numbers = [100, 42, 16, 10]
+
+console.log('-----------------')
+printIt(numbers, double)
+console.log('-----------------')
+printIt(numbers, square)
