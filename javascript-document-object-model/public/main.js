85541bc71af53b628880a30b28fe5fb5b4c655fe
Gavin Stark <gavin@gstark.com>
2020-09-16 11:59:27 -0400

    Basic clicking handling for our tic tac toe game

--- javascript-document-object-model/public/main.js
+++ javascript-document-object-model/public/main.js
@@ -6,9 +6,36 @@
 
 // document.addEventListener('DOMContentLoaded', main)
 
+let theTextWeWillPutInTheSquareWhenClicked = 'X'
+
 function handleClickSquare(event) {
+  // Figure out what was clicked on
   const thingClickedOn = event.target
-  thingClickedOn.textContent = 'X'
+
+  // Before we do anything...
+  //
+  // Lets check if the square is already taken!
+  //
+  // if it is, then console.log a message, otherwise, handle the change to the cell
+  if (thingClickedOn.classList.contains('taken')) {
+    console.log('NOPES!')
+    thingClickedOn.classList.add('not-allowed-click')
+  } else {
+    // Change the text content!
+    thingClickedOn.textContent = theTextWeWillPutInTheSquareWhenClicked
+    thingClickedOn.classList.add('taken')
+
+    // Let's change the player to the opposite to give them a turn
+
+    // If the current player is exactly the text X
+    if (theTextWeWillPutInTheSquareWhenClicked === 'X') {
+      // Then *ASSIGN* the text 'O' to current player
+      theTextWeWillPutInTheSquareWhenClicked = 'O'
+    } else {
+      // Otherwise we were already an 'O', so become 'X'
+      theTextWeWillPutInTheSquareWhenClicked = 'X'
+    }
+  }
 }
 
 const allSquares = document.querySelectorAll('li')
