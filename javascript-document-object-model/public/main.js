a1981f127ae46edde1cc546d4c5739e58ce0aa6d
Gavin Stark <gavin@gstark.com>
2020-09-16 11:47:00 -0400

    Added basic click handling on our squares

--- javascript-document-object-model/public/main.js
+++ javascript-document-object-model/public/main.js
@@ -1,7 +1,18 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+// function main() {
+//   if (document.querySelector('h1.hello-world')) {
+//     document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+//   }
+// }
+
+// document.addEventListener('DOMContentLoaded', main)
+
+function handleClickSquare(event) {
+  const thingClickedOn = event.target
+  thingClickedOn.textContent = 'X'
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const allSquares = document.querySelectorAll('li')
+
+allSquares.forEach(element =>
+  element.addEventListener('click', handleClickSquare)
+)
