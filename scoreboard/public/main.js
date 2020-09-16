759a63b64492dc8130273c64a400bc315071df0f
Gavin Stark <gavin@gstark.com>
2020-09-16 15:38:16 -0400

    Example of using input event on an input element

--- scoreboard/public/main.js
+++ scoreboard/public/main.js
@@ -1,7 +1,25 @@
+// The function to call when the user types in
+// the input field for team one
+function handleChangeTeamOneName(event) {
+  // Get the value of the input and put that in a variable
+  const elementThatChanged = event.target
+  const inputFieldValue = elementThatChanged.value
+
+  // Get the element for the team name
+  const teamNameElement = document.querySelector('.team1 h2')
+
+  // Set that element's textContent to the variable
+  teamNameElement.textContent = inputFieldValue
+}
+
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  // This code runs once the page is totally loaded
+
+  // Give me an input that is a child anywhere below something
+  // that has a class of `team1`
+
+  const teamOneInput = document.querySelector('.team1 input')
+  teamOneInput.addEventListener('input', handleChangeTeamOneName)
 }
 
 document.addEventListener('DOMContentLoaded', main)
