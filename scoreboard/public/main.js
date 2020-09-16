282dcf25da9075b42c4e5ff0d4ab64f347b05a07
Gavin Stark <gavin@gstark.com>
2020-09-16 15:42:11 -0400

    All the code inside main and only fetching teamNameElement once

--- scoreboard/public/main.js
+++ scoreboard/public/main.js
@@ -1,18 +1,20 @@
-// The function to call when the user types in
-// the input field for team one
-function handleChangeTeamOneName(event) {
-  // Get the value of the input and put that in a variable
-  const elementThatChanged = event.target
-  const inputFieldValue = elementThatChanged.value
-
+function main() {
   // Get the element for the team name
   const teamNameElement = document.querySelector('.team1 h2')
 
-  // Set that element's textContent to the variable
-  teamNameElement.textContent = inputFieldValue
-}
+  console.log(teamNameElement)
 
-function main() {
+  // The function to call when the user types in
+  // the input field for team one
+  function handleChangeTeamOneName(event) {
+    // Get the value of the input and put that in a variable
+    const elementThatChanged = event.target
+    const inputFieldValue = elementThatChanged.value
+
+    // Set that element's textContent to the variable
+    teamNameElement.textContent = inputFieldValue
+  }
+
   // This code runs once the page is totally loaded
 
   // Give me an input that is a child anywhere below something
