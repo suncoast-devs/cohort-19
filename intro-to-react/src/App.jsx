ef97da476225ddaa0567bfff66a0824a8dfc3d02
Gavin Stark <gavin@gstark.com>
2020-09-21 11:19:09 -0400

    Creating our first component

--- intro-to-react/src/App.jsx
+++ intro-to-react/src/App.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react'
 
+/* Create a component who's job it is
+   to display whatever we call a "Greeting"
+   in our application */
+class Greeting extends Component {
+  render() {
+    return <p>Hello, Gavin!</p> /* JSX, HTML looking thing */
+  }
+}
+
 class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    // return "<div>Hello, React</div>
+    return (
+      <div>
+        <Greeting></Greeting>
+        <Greeting></Greeting>
+        <Greeting></Greeting>
+        <Greeting></Greeting>
+        <Greeting></Greeting>
+        <Greeting></Greeting>
+        <Greeting></Greeting>
+      </div>
+    )
   }
 }
 
