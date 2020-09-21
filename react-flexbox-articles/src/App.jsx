94b67e812e59a721a62a189d280cd8d173a8d9f0
Gavin Stark <gavin@gstark.com>
2020-09-21 15:33:52 -0400

    OneArticle component now lets us specify the title for each USAGE of the component

--- react-flexbox-articles/src/App.jsx
+++ react-flexbox-articles/src/App.jsx
@@ -31,42 +31,38 @@ class PageFooter extends Component {
   }
 }
 
+class OneArticle extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        <p>
+          Bacon ipsum dolor amet ham hock capicola kevin, brisket t-bone pork
+          chop meatball ground round shoulder ribeye bacon. Meatloaf ground
+          round bacon turkey pork belly biltong andouille tenderloin buffalo
+          jowl landjaeger fatback strip steak tongue. Corned beef fatback ground
+          round buffalo, t-bone spare ribs chicken salami tri-tip prosciutto
+          kielbasa beef ribs. Short ribs flank beef, andouille chuck pork belly
+          pastrami turkey pork loin fatback salami. Nulla pork belly ball tip,
+          lorem nostrud adipisicing pancetta consectetur beef. Pig sausage
+          pastrami andouille. Bresaola ribeye short ribs sausage tri-tip
+          landjaeger. Beef ribs pork shankle jerky sint shank eu corned beef
+          aute deserunt.
+        </p>
+        <a href="#">read more</a>
+      </article>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <>
         <PageHeader />
         <main>
-          <article>
-            <h2>Article Title Here</h2>
-            <p>
-              Bacon ipsum dolor amet ham hock capicola kevin, brisket t-bone
-              pork chop meatball ground round shoulder ribeye bacon. Meatloaf
-              ground round bacon turkey pork belly biltong andouille tenderloin
-              buffalo jowl landjaeger fatback strip steak tongue. Corned beef
-              fatback ground round buffalo, t-bone spare ribs chicken salami
-              tri-tip prosciutto kielbasa beef ribs. Short ribs flank beef,
-              andouille chuck pork belly pastrami turkey pork loin fatback
-              salami. Nulla pork belly ball tip, lorem nostrud adipisicing
-              pancetta consectetur beef. Pig sausage pastrami andouille.
-              Bresaola ribeye short ribs sausage tri-tip landjaeger. Beef ribs
-              pork shankle jerky sint shank eu corned beef aute deserunt.
-            </p>
-            <a href="#">read more</a>
-          </article>
-          <article>
-            <h2>Article Title Here</h2>
-            <p>
-              Bacon ipsum dolor amet capicola chicken shank alcatra dolore
-              frankfurter minim aute. Commodo in proident ut consectetur spare
-              ribs ball tip frankfurter doner kielbasa meatloaf ex short ribs
-              exercitation chuck. Pancetta in ham boudin andouille jerky id
-              officia cupim aliquip laborum incididunt. In mollit alcatra velit
-              sunt sirloin exercitation voluptate kevin enim in. Flank laborum
-              salami ullamco filet mignon quis duis pig.
-            </p>
-            <a href="#">read more</a>
-          </article>
+          <OneArticle title="First Article" />
+          <OneArticle title="Second Article" />
           <article>
             <h2>Article Title Here</h2>
             <p>
