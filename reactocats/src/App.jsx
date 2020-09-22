250b68d8677b836fc9124db089e8452aeea5a937
Gavin Stark <gavin@gstark.com>
2020-09-22 09:58:40 -0400

    Organized the components into folders

--- reactocats/src/App.jsx
+++ reactocats/src/App.jsx
@@ -1,59 +1,6 @@
 import React, { Component } from 'react'
-
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">
-                <img
-                  src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-                  alt="GithubLogo"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">Octodex</a>
-            </li>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="https://octodex.github.com/faq/">FAQ</a>
-            </li>
-          </ul>
-        </nav>
-
-        <nav>
-          <ul>
-            <li>
-              <a href="https://twitter.com/githubdesign">
-                Follow us on Twitter
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/">Back to GitHub.com</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    )
-  }
-}
-
-class Footer extends Component {
-  render() {
-    return (
-      <footer>
-        <div>
-          <p>© 2013 – 2020 GitHub, Inc. All rights reserved.</p>
-        </div>
-      </footer>
-    )
-  }
-}
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
 
 class App extends Component {
   render() {
---
+++ reactocats/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div>
+          <p>© 2013 – 2020 GitHub, Inc. All rights reserved.</p>
+        </div>
+      </footer>
+    )
+  }
+}
