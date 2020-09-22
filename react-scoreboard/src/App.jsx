662bb861fe0b5f316b45a6b7e9a62eef83913612
Gavin Stark <gavin@gstark.com>
2020-09-22 11:14:58 -0400

    Creates a changable STATE property we can manage.

--- react-scoreboard/src/App.jsx
+++ react-scoreboard/src/App.jsx
@@ -1,5 +1,32 @@
 import React, { Component } from 'react'
 
+class Team extends Component {
+  // This property MUST be named 'state'
+  // It *should* be an object
+  // The keys can be anything we desire, we name those.
+  state = {
+    score: 0,
+    name: 'Team 1',
+  }
+
+  render() {
+    return (
+      <section className="team1">
+        <h2>{this.state.name}</h2>
+        <h3>{this.state.score}</h3>
+        <fieldset>
+          <input type="text" placeholder="Name" />
+        </fieldset>
+
+        <fieldset>
+          <i className="add fas fa-2x fa-plus-circle"></i>
+          <i className="subtract fas fa-2x fa-minus-circle"></i>
+        </fieldset>
+      </section>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -8,18 +35,7 @@ class App extends Component {
           <h1>My Score Board</h1>
         </header>
         <main>
-          <section className="team1">
-            <h2>Team 1</h2>
-            <h3>0</h3>
-            <fieldset>
-              <input type="text" placeholder="Name" />
-            </fieldset>
-
-            <fieldset>
-              <i className="add fas fa-2x fa-plus-circle"></i>
-              <i className="subtract fas fa-2x fa-minus-circle"></i>
-            </fieldset>
-          </section>
+          <Team />
 
           <section className="team2">
             <h2>Team 2</h2>
