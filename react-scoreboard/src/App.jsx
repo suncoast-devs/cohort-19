ba11047b835b9dfc92ae3b0bf5b6df9759b65e04
Gavin Stark <gavin@gstark.com>
2020-09-22 11:30:29 -0400

    Updates the state every time we call setState with a new value for score

--- react-scoreboard/src/App.jsx
+++ react-scoreboard/src/App.jsx
@@ -9,6 +9,26 @@ class Team extends Component {
     name: 'Team 1',
   }
 
+  handleClickOnAddButton = event => {
+    // This is just like our event handlers from
+    // when we used addEventListener. EXCEPT, we
+    // define them as arrow function style.
+
+    const theCurrentScore = this.state.score
+    console.log(`CLICKED ON THE BUTTON and the score was ${theCurrentScore}`)
+
+    const increasedScore = theCurrentScore + 1
+
+    // This is an illegal line of code
+    // this.state.score = increasedScore
+
+    // The only way to change state is with
+    // the function this.setState -- We give
+    // it an object with new key and value
+    // we want to change
+    this.setState({ score: increasedScore })
+  }
+
   render() {
     return (
       <section className="team1">
@@ -19,7 +39,10 @@ class Team extends Component {
         </fieldset>
 
         <fieldset>
-          <i className="add fas fa-2x fa-plus-circle"></i>
+          <i
+            className="add fas fa-2x fa-plus-circle"
+            onClick={this.handleClickOnAddButton}
+          ></i>
           <i className="subtract fas fa-2x fa-minus-circle"></i>
         </fieldset>
       </section>
