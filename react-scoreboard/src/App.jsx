506c9358744e1394054499499b57045e8f8b03f7
Gavin Stark <gavin@gstark.com>
2020-09-22 11:05:59 -0400

    Bring in the HTML and CSS for the scoreboard

--- react-scoreboard/src/App.jsx
+++ react-scoreboard/src/App.jsx
@@ -2,7 +2,40 @@ import React, { Component } from 'react'
 
 class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <>
+        <header>
+          <h1>My Score Board</h1>
+        </header>
+        <main>
+          <section class="team1">
+            <h2>Team 1</h2>
+            <h3>0</h3>
+            <fieldset>
+              <input type="text" placeholder="Name" />
+            </fieldset>
+
+            <fieldset>
+              <i class="add fas fa-2x fa-plus-circle"></i>
+              <i class="subtract fas fa-2x fa-minus-circle"></i>
+            </fieldset>
+          </section>
+
+          <section class="team2">
+            <h2>Team 2</h2>
+            <h3>0</h3>
+            <fieldset>
+              <input type="text" placeholder="Name" />
+            </fieldset>
+
+            <fieldset>
+              <i class="add fas fa-2x fa-plus-circle"></i>
+              <i class="subtract fas fa-2x fa-minus-circle"></i>
+            </fieldset>
+          </section>
+        </main>
+      </>
+    )
   }
 }
 
