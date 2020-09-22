e752ca70e51ec2c53ee26a91f608e9c4800de81a
Gavin Stark <gavin@gstark.com>
2020-09-22 10:29:46 -0400

    Now the data is even in a separate file!

--- reactocats/src/App.jsx
+++ reactocats/src/App.jsx
@@ -2,38 +2,11 @@ import React, { Component } from 'react'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Octocat } from './components/Octocat'
+import { octocats } from './octocats'
 
 class App extends Component {
   render() {
     // Data
-    const octocats = [
-      {
-        number: 150,
-        name: 'Octoqueer',
-        image: 'https://octodex.github.com//images/Octoqueer.png',
-        link: 'https://octodex.github.com//octoqueer/',
-        authorLink: 'https://github.com/cameronfoxly',
-        authorImage: 'https://github.com/cameronfoxly.png',
-      },
-      {
-        name: 'Terracottocat',
-        link: 'https://octodex.github.com//terracottocat/',
-        image: 'https://octodex.github.com//images/Terracottocat_Single.png',
-        number: '149',
-        authorLink: 'https://github.com/chubbmo',
-        authorImage: 'https://github.com/chubbmo.png',
-        authorName: 'chubbmo',
-      },
-      {
-        name: 'Octogatos',
-        link: 'https://octodex.github.com//octogatos/',
-        image: 'https://octodex.github.com//images/Octogatos.png',
-        number: '148',
-        authorLink: 'https://github.com/cameronfoxly',
-        authorImage: 'https://github.com/cameronfoxly.png',
-        authorName: 'cameronfoxly',
-      },
-    ]
 
     // Take an array of octocat objects (from above)
     //
