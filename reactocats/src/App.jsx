c96a178dda913345129640475fa0b298207ff019
Gavin Stark <gavin@gstark.com>
2020-09-22 09:50:39 -0400

    Extracted Header component

--- reactocats/src/App.jsx
+++ reactocats/src/App.jsx
@@ -1,45 +1,53 @@
 import React, { Component } from 'react'
 
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">
+                <img
+                  src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                  alt="GithubLogo"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#">Octodex</a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="https://octodex.github.com/faq/">FAQ</a>
+            </li>
+          </ul>
+        </nav>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="https://twitter.com/githubdesign">
+                Follow us on Twitter
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/">Back to GitHub.com</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <>
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <a href="#">
-                  <img
-                    src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-                    alt="GithubLogo"
-                  />
-                </a>
-              </li>
-              <li>
-                <a href="#">Octodex</a>
-              </li>
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="https://octodex.github.com/faq/">FAQ</a>
-              </li>
-            </ul>
-          </nav>
-
-          <nav>
-            <ul>
-              <li>
-                <a href="https://twitter.com/githubdesign">
-                  Follow us on Twitter
-                </a>
-              </li>
-              <li>
-                <a href="https://github.com/">Back to GitHub.com</a>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <Header />
 
         <main>
           <article>
