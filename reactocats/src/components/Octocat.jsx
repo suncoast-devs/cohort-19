31cc5194e64d298eb04ea71ca03979ee8018af55
Gavin Stark <gavin@gstark.com>
2020-09-22 14:06:21 -0400

    Cleanup HTML and CSS

--- reactocats/src/components/Octocat.jsx
+++ reactocats/src/components/Octocat.jsx
@@ -13,22 +13,24 @@ export class Octocat extends Component {
               alt={this.props.name}
             />
           </a>
-        </figure>
-        <ul>
-          <li>
-            #{this.props.number}:
-            <a href={this.props.link}>
-              <strong>{this.props.name}</strong>
-            </a>
-          </li>
-          {this.props.authors.map(author => (
-            <li>
-              <a href={author.link}>
-                <img src={author.image} width="24" height="24" alt="" />
+          <figcaption>
+            <p>
+              #{this.props.number}:&nbsp;
+              <a href={this.props.link}>
+                <strong>{this.props.name}</strong>
               </a>
-            </li>
-          ))}
-        </ul>
+            </p>
+            <ul>
+              {this.props.authors.map(author => (
+                <li>
+                  <a href={author.link}>
+                    <img src={author.image} width="24" height="24" alt="" />
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </figcaption>
+        </figure>
       </article>
     )
   }
