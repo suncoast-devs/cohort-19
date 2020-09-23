403597fe74a83d0bb047eac37c8d2ea00f276438
Gavin Stark <gavin@gstark.com>
2020-09-23 11:33:00 -0400

    Dynamically displaying the list of people in space from DYNAMIC data from an API

--- react-space/src/App.jsx
+++ react-space/src/App.jsx
@@ -1,24 +1,37 @@
 import React, { Component } from 'react'
-import spaceData from './space.json'
 
 class App extends Component {
-  state = spaceData
+  state = {
+    number: 0,
+    people: [],
+  }
+
+  async componentDidMount() {
+    const response = await fetch('http://api.open-notify.org/astros.json')
+    const apiJSON = await response.json()
+    this.setState(apiJSON)
+  }
 
   render() {
     console.log(this.state)
 
-    return (
-      <div>
-        <p>There are {this.state.number} people in space right now!</p>
-        <ul>
-          {this.state.people
-            .filter(person => person.craft === 'ISS')
-            .map(person => (
-              <li key={person.name}>{person.name}</li>
-            ))}
-        </ul>
-      </div>
-    )
+    if (this.state.number === 0) {
+      // Nobody in space: render NOTHING
+      return <></>
+    } else {
+      return (
+        <div>
+          <p>There are {this.state.number} people in space right now!</p>
+          <ul>
+            {this.state.people
+              .filter(person => person.craft === 'ISS')
+              .map(person => (
+                <li key={person.name}>{person.name}</li>
+              ))}
+          </ul>
+        </div>
+      )
+    }
   }
 }
 
