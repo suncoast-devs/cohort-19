c879f0f74f0e8d22fbdae8497d0827126706a016
Gavin Stark <gavin@gstark.com>
2020-09-23 11:16:24 -0400

    Initial mockup of our space app

---
+++ react-space/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>There are 5 people in space right now!</p>
+        <ul>
+          <li>Bob</li>
+          <li>Sally</li>
+          <li>Jane</li>
+          <li>Barbara</li>
+          <li>Tim</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
