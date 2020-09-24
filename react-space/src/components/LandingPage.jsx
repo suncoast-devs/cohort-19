0621a240b0439c49632c39aeb8f368d564e9b89f
Gavin Stark <gavin@gstark.com>
2020-09-24 10:48:24 -0400

    Adds extra URLs

--- react-space/src/components/LandingPage.jsx
+++ react-space/src/components/LandingPage.jsx
@@ -12,6 +12,15 @@ export class LandingPage extends Component {
           <li>
             <a href="/iss">When can I see the ISS?</a>
           </li>
+          <li>
+            <a href="/iss/boston">When can I see the ISS in Boston?</a>
+          </li>
+          <li>
+            <a href="/iss/la">When can I see the ISS in LA?</a>
+          </li>
+          <li>
+            <a href="/iss/london">When can I see the ISS in London?</a>
+          </li>
         </ul>
       </div>
     )
