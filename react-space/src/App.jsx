98494497d91efdb4952c413cc0dba01ef9189ac3
Gavin Stark <gavin@gstark.com>
2020-09-24 09:45:45 -0400

    Added cors-anywhere to work around the CORS limitation of this API

--- react-space/src/App.jsx
+++ react-space/src/App.jsx
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
 import { PeopleInSpaceRightNow } from './components/PeopleInSpaceRightNow'
 
+class InternationalSpaceStationPassTimes extends Component {
+  state = {
+    request: {
+      passes: 0,
+    },
+    response: [],
+  }
+
+  async componentDidMount() {
+    const response = await fetch(
+      'https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=27.770840&lon=-82.660810'
+    )
+    const apiData = await response.json()
+
+    this.setState(apiData)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>There are {this.state.request.passes} passes of the space station</p>
+        <ul>
+          {this.state.response.map(pass => (
+            <li>
+              For {pass.duration} at {pass.risetime}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
-    return <PeopleInSpaceRightNow />
+    return (
+      <>
+        <PeopleInSpaceRightNow />
+        <InternationalSpaceStationPassTimes />
+      </>
+    )
   }
 }
 
