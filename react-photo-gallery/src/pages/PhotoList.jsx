e77ba21a172262e2664576285419b0ba25d6a231
Gavin Stark <gavin@gstark.com>
2020-09-28 10:32:43 -0400

    Mdae the photo list page dynamic

--- react-photo-gallery/src/pages/PhotoList.jsx
+++ react-photo-gallery/src/pages/PhotoList.jsx
@@ -2,13 +2,41 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import photoData from '../photos.json'
 
+class PhotoListCard extends Component {
+  render() {
+    return (
+      <div className="column is-one-third">
+        <div className="card">
+          <div className="card-content">
+            <div className="card-image">
+              <figure className="image">
+                <Link to={`${this.props.category}/${this.props.index}`}>
+                  <img src={this.props.imageURL} alt={this.props.title} />
+                </Link>
+                <figcaption>
+                  <Link to={`${this.props.category}/${this.props.index}`}>
+                    {this.props.title}
+                  </Link>
+                </figcaption>
+              </figure>
+            </div>
+            <div className="content" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 export class PhotoList extends Component {
   render() {
     const category = this.props.match.params.category
 
     const photoListData = photoData[category]
 
-    console.log(photoListData)
+    const photos = photoListData.photos
+
+    console.log(photos)
 
     return (
       <div className="container">
@@ -23,106 +51,22 @@ export class PhotoList extends Component {
               </Link>
             </li>
             <li className="is-active">
-              <Link to={'/pandas'}>Panda Bears</Link>
+              <Link to={'/pandas'}>{photoListData.title}</Link>
             </li>
           </ul>
         </nav>
-        <h3 className="title">Panda Bears</h3>
-        <h4 className="subtitle">
-          Pandas are bears native to south-central China, and are objectively
-          the cutest animals on earth.
-        </h4>
+        <h3 className="title">{photoListData.title}</h3>
+        <h4 className="subtitle">{photoListData.description}</h4>
         <div className="columns is-multiline">
-          <div className="column is-one-third">
-            <div className="card">
-              <div className="card-content">
-                <div className="card-image">
-                  <figure className="image">
-                    <Link to="/pandas/0">
-                      <img
-                        src="https://things-i-like.netlify.app/images/baby_panda.jpg"
-                        alt="Panda Waving"
-                      />
-                    </Link>
-                    <figcaption>
-                      <Link to="/pandas/0">Panda Waving</Link>
-                    </figcaption>
-                  </figure>
-                </div>
-                <div className="content" />
-              </div>
-            </div>
-          </div>
-
-          <div className="column is-one-third">
-            <div className="card">
-              <div className="card-content">
-                <div className="card-image">
-                  <figure className="image">
-                    <Link to="/pandas/1">
-                      <img
-                        src="https://things-i-like.netlify.app/images/grosser_panda.jpg"
-                        alt="Großer Panda im Ocean Park, Hongkong"
-                      />
-                    </Link>
-                    <figcaption>
-                      <Link to="/pandas/1">
-                        Großer Panda im Ocean Park, Hongkong
-                      </Link>
-                    </figcaption>
-                  </figure>
-                </div>
-                <div className="content" />
-              </div>
-            </div>
-          </div>
-
-          <div className="column is-one-third">
-            <div className="card">
-              <div className="card-content">
-                <div className="card-image">
-                  <figure className="image">
-                    <Link to="/pandas/2">
-                      <img
-                        src="https://things-i-like.netlify.app/images/tree_panda.png"
-                        alt="Wild pandas get a boost; the iconic species has been upgraded from 'endangered' to 'vulnerable.'"
-                      />
-                    </Link>
-                    <figcaption>
-                      <Link to="/pandas/2">
-                        Wild pandas get a boost; the iconic species has been
-                        upgraded from 'endangered' to 'vulnerable.'
-                      </Link>
-                    </figcaption>
-                  </figure>
-                </div>
-                <div className="content" />
-              </div>
-            </div>
-          </div>
-
-          <div className="column is-one-third">
-            <div className="card">
-              <div className="card-content">
-                <div className="card-image">
-                  <figure className="image">
-                    <Link to="/pandas/3">
-                      <img
-                        src="https://things-i-like.netlify.app/images/rising_sun.png"
-                        alt="Sacred Warrior, by Adrian Smith"
-                      />
-                    </Link>
-                    <figcaption>
-                      <Link to="/pandas/3">
-                        Sacred Warrior, by Adrian Smith
-                      </Link>
-                    </figcaption>
-                  </figure>
-                </div>
-                <div className="content" />
-              </div>
-            </div>
-          </div>
+          {photos.map((photo, index) => (
+            <PhotoListCard
+              key={photo.title}
+              imageURL={photo.imageURL}
+              title={photo.title}
+              category={category}
+              index={index}
+            />
+          ))}
         </div>
       </div>
     )
