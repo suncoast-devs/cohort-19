2bb173626c79e1b5aa3c94d44fd36b53981ca114
Gavin Stark <gavin@gstark.com>
2020-09-29 10:30:20 -0400

    Added a state named usedLetters to track what letters are used
    Added a function addLetterToUsedLetters to record a letter when we need.
    
    Supplied that function to our AlphabetLetter component to be called when a letter is clicked.

--- snowman/src/App.jsx
+++ snowman/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import snowman_0 from './images/snowman/step_0.png'
 import snowman_1 from './images/snowman/step_1.png'
 import snowman_2 from './images/snowman/step_2.png'
@@ -9,14 +9,35 @@ import snowman_6 from './images/snowman/step_6.png'
 import snowman_7 from './images/snowman/step_7.png'
 
 function AlphabetLetter(props) {
+  function onClickedLetter() {
+    // Handle the click
+    props.addLetterToUsedLetters(props.letter)
+  }
+
   if (props.used) {
     return <li className="used">{props.letter}</li>
   } else {
-    return <li>{props.letter}</li>
+    return <li onClick={onClickedLetter}>{props.letter}</li>
   }
 }
 
 function App() {
+  // STATES GO HERE
+
+  // Create a state to hold the list of used letters
+  const [usedLetters, setUsedLetters] = useState([])
+
+  function addLetterToUsedLetters(letter) {
+    // Update the state to include this new letter
+
+    // Use the spread operator to make usedLetters a list of the EXISTING letters
+    // and then tack on this new letter
+    const newUsedLetters = [...usedLetters, letter]
+
+    // use our Hook function setUsedLetters to update the state
+    setUsedLetters(newUsedLetters)
+  }
+
   const snowmen = [
     snowman_0,
     snowman_1,
@@ -74,8 +95,9 @@ function App() {
         {allTheLetters.map((individualLetter) => (
           <AlphabetLetter
             key={individualLetter}
-            used={false}
+            used={usedLetters.includes(individualLetter)}
             letter={individualLetter}
+            addLetterToUsedLetters={addLetterToUsedLetters}
           />
         ))}
       </ul>
