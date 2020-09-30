ba4a2a077d77a0369e46277e77a77e4dfa09e7ca
Gavin Stark <gavin@gstark.com>
2020-09-30 10:06:56 -0400

    Added a state to keep track of the current value in the display

--- react-calculator/src/components/Calculator.jsx
+++ react-calculator/src/components/Calculator.jsx
@@ -1,11 +1,12 @@
-
-import React from 'react'
+import React, { useState } from 'react'
 
 export function Calculator() {
+  const [display, setDisplay] = useState(42)
+
   return (
     <main>
       <div className="calculator">
-        <div className="display">0</div>
+        <div className="display">{display}</div>
         <div className="buttons">
           <button className="button fn">AC</button>
           <button className="button fn">&#177;</button>
