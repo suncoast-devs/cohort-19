77f1ed736dfe00266b72dbc8c166b4b5d0ba2f94
Gavin Stark <gavin@gstark.com>
2020-10-08 10:54:22 -0400

    Updated our App to start using react router to route to the two pages we have worked on

--- TacoTuesday/ClientApp/src/App.jsx
+++ TacoTuesday/ClientApp/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
 import './custom.scss'
 import { Restaurants } from './pages/Restaurants'
 import { NewRestaurant } from './pages/NewRestaurant'
 
 export function App() {
-  return <NewRestaurant />
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Restaurants />
+      </Route>
+      <Route exact path="/new">
+        <NewRestaurant />
+      </Route>
+    </Switch>
+  )
 }
