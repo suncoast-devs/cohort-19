6f1174bd96d3474c88f960ae5e4bf928b794f67f
Gavin Stark <gavin@gstark.com>
2020-11-09 11:36:52 -0500

    First passing test

--- bowling-tdd/__tests__/bowling_test.js
+++ bowling-tdd/__tests__/bowling_test.js
@@ -1,3 +1,31 @@
-it('Name of the test', () => {
-  // body of the test
+class BowlingGame {
+  // Nothing here yet
+
+  roll() {
+    // nothing here yet
+  }
+
+  score() {
+    return 0
+  }
+}
+
+it('The score of a game of bowling with all gutter balls is 0', () => {
+  // Setup
+  // Execute
+  // Expectation
+
+  // Setup
+  const bowlingGame = new BowlingGame()
+
+  // Setup
+  for (let rollIndex = 0; rollIndex < 20; rollIndex++) {
+    bowlingGame.roll(0)
+  }
+
+  // Execute
+  const totalScore = bowlingGame.score()
+
+  // Expectation
+  expect(totalScore).toBe(0)
 })
